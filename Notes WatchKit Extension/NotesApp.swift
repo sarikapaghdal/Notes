1bcdfb42c3c3af68a1052939c5ac73ddc1cbabbf
Dhaval Shekhada <sarika.paghdal1@gmail.com>
2022-04-04 12:07:33 +0200

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Sarika on 04.04.22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
